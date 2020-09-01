94ee3f0224f0e5377fbde771f2d3c5505cdf8ccf
SugarF0x <malion.torchwood@yandex.ru>
2020-09-01 21:15:16 +0300

    :corn: Remove obsolete initial commit files
    > Remove files that will not be in use
    > Leave jest file as a sample while im still learning

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,13 @@
-import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+/**
+ * i keep this commented just as a sample of how to test components and perhaps pages
+ */
 
-describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
+// import { mount } from '@vue/test-utils'
+// import Logo from '@/components/Logo.vue'
+//
+// describe('Logo', () => {
+//   test('is a Vue instance', () => {
+//     const wrapper = mount(Logo)
+//     expect(wrapper.vm).toBeTruthy()
+//   })
+// })
