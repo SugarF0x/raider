670993454e221b54a469f73cae49307908cbf629
Sugar_F0x <sugarfox.dev@gmail.com>
2020-12-06 16:13:46 +0300

    :corn: Update NPM packages
    Some newly introduced features would be much welcome for several
      packages, so i guess it's just about time to update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'spa',
+  ssr: false,
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
