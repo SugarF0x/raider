f0c50797ab009726bda519955cfbb9c42359e574
Sugar_F0x <sugarfox.dev@gmail.com>
2020-12-18 19:46:05 +0300

    :hammer_and_wrench: Default layout project error due to missing script

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,15 @@
   </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'default'
+})
+
+</script>
+
 <style lang="less" scoped>
   .container {
     height: 100vh;
