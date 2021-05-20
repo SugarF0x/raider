5b43559eff7139dd30ab04f868c46d5175ad45d9
Sugar_F0x <sugarfox.dev@gmail.com>
2021-05-20 21:26:37 +0300

    :white_check_mark: Add health HUD piece

--- components/Hud/Health.vue
+++ components/Hud/Health.vue
@@ -1,13 +1,57 @@
 <template lang="pug">
-
+  v-group
+    v-image(:config="healthConfig")
+    util-text(:config="{ x: 330, y: 700, fill: 'yellow', fontSize: 20, text: `${health}/${totalHealth}` }")
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { computed, defineComponent, onMounted, onUnmounted } from "@nuxtjs/composition-api"
+import { parseMarkdown } from "~/assets/utils"
+import { HEALTH } from "~/assets/consts/markdowns/hud"
+import { useAccessor } from "~/assets/hooks"
+import { EXPERIENCE_THRESHOLD } from "~/assets/consts/balance"
 
-export default Vue.extend({
-  name: "Health"
+export default defineComponent({
+  setup() {
+    const { character } = useAccessor()
+    const health = computed(() => character.health)
+    const totalHealth = computed(() => character.totalHealth)
+    const fill = computed(() => health.value/totalHealth.value)
+
+    const healthConfig = computed(() => getConfig(fill.value))
+
+    let interval: NodeJS.Timeout
+    onMounted(() => {
+      interval = setInterval(() => {
+        let newValue = health.value - 1
+        if (newValue <= 0) newValue = totalHealth.value
+        character.SET_HEALTH(newValue)
+      }, 100)
+    })
+
+    onUnmounted(() => {
+      clearInterval(interval)
+    })
+
+    return {
+      health,
+      totalHealth,
+      healthConfig
+    }
+  }
 })
+
+function getConfig(fill: number) {
+  const health = parseMarkdown(HEALTH)
+  if (!health.crop) throw new Error('Health markdown is to contain tileset option')
+
+  health.y += health.height * (1 - fill)
+  health.height *= fill
+  health.crop.y += health.crop.height * (1 - fill)
+  health.crop.height *= fill
+
+  return health
+}
 </script>
 
 <style lang="sass" scoped>
--- store/character.ts
+++ store/character.ts
@@ -3,6 +3,7 @@ import { getterTree, mutationTree, actionTree } from 'typed-vuex'
 const defaultState = () => ({
   gold: 0,
   armor: 3,
+  health: 50,
   upgrade: 0,
   experience: 0
 })
@@ -12,7 +13,8 @@ export const state = () => (defaultState())
 export const getters = getterTree(state, {
   // TODO: update these once equipment is added
   totalArmor: state => 3,
-  totalAttack: state => 1
+  totalAttack: state => 1,
+  totalHealth: state => 50
 })
 
 export const mutations = mutationTree(state, {
@@ -20,7 +22,8 @@ export const mutations = mutationTree(state, {
   SET_GOLD: (state, value: number) => { state.gold = value },
   SET_ARMOR: (state, value: number) => { state.armor = value },
   SET_UPGRADE: (state, value: number) => { state.upgrade = value },
-  SET_EXPERIENCE: (state, value: number) => { state.experience = value }
+  SET_EXPERIENCE: (state, value: number) => { state.experience = value },
+  SET_HEALTH: (state, value: number) => { state.health = value },
 })
 
 export const actions = actionTree({ state, getters, mutations }, {})
