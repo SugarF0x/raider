5070c4b6c8106a912cd59ba9ed8ba0945d4a802b
Sugar_F0x <sugarfox.dev@gmail.com>
2021-05-20 20:32:51 +0300

    :recycle: Refactor Gold utils into Gold component
    There is not enough logic for it to need destructure

--- components/Hud/Gold.vue
+++ components/Hud/Gold.vue
@@ -12,7 +12,8 @@
 <script lang="ts">
 import { defineComponent, computed, onMounted, onUnmounted } from "@nuxtjs/composition-api"
 import { useAccessor } from "~/assets/hooks"
-import { getColumn } from "~/components/Hud/Gold/utils"
+import { parseMarkdown } from "~/assets/utils"
+import { GOLD } from "~/assets/consts/markdowns/hud"
 
 export default defineComponent({
   setup() {
@@ -52,4 +53,16 @@ export default defineComponent({
     }
   },
 })
+
+function getColumn(column: number, fill: number) {
+  const gold = parseMarkdown(GOLD[column])
+  if (!gold.crop) throw new Error('Gold column markdown is to contain tileset option')
+
+  gold.y = gold.y + (gold.height * (1 - fill))
+  gold.height = gold.height * fill
+  gold.crop.y = gold.crop.y + (20 - 20 * fill) * 3
+  gold.crop.height = gold.crop.height - (20 - 20 * fill) * 3
+
+  return gold
+}
 </script>
