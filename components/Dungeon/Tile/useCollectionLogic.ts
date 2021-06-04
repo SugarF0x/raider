6e6aee7d35eb0e2a5369adb4826f0dbcd473a2ec
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-04 13:29:37 +0300

    :white_check_mark: Add shield collection logic

--- components/Dungeon/Tile/useCollectionLogic.ts
+++ components/Dungeon/Tile/useCollectionLogic.ts
@@ -9,8 +9,27 @@ export function useCollectionLogic(tile: Ref<Tile>) {
   switch(tile.value.type) {
     case "coin": return () => {
       const newGoldAmount = character.gold + 1
+
       if (newGoldAmount >= 100) character.SET_GOLD(newGoldAmount - 100)
       else character.SET_GOLD(newGoldAmount)
+
+      instance.INC_SCORE(1)
+      dungeon.REMOVE_TILE(tile.value.id)
+    }
+    case "shield": return () => {
+      const newArmorValue = character.armor + 1
+      const excessArmorValue = newArmorValue - character.totalArmor
+
+      if (newArmorValue <= character.totalArmor) {
+        character.SET_ARMOR(newArmorValue)
+      } else {
+        character.SET_ARMOR(character.totalArmor)
+
+        const newUpgradeValue = character.upgrade + excessArmorValue
+        if (newUpgradeValue >= 100) character.SET_UPGRADE(newUpgradeValue - 100)
+        else character.SET_UPGRADE(newUpgradeValue)
+      }
+
       instance.INC_SCORE(1)
       dungeon.REMOVE_TILE(tile.value.id)
     }
