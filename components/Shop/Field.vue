47cc94d879973c6f30027e9ad62bb7b16521af31
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-10 20:17:03 +0300

    :construction: Add base for shop fields display

---
+++ components/Shop/Field.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+  v-group
+    v-group(v-if="type === FieldType.SELECTED")
+      v-image(:config="selectedItemBackgroundConfig.begin")
+      v-image(:config="selectedItemBackgroundConfig.mid")
+      v-image(:config="selectedItemBackgroundConfig.end")
+    v-group(v-else-if="type === FieldType.EQUIPPED")
+      v-image(:config="currentItemBackgroundConfig.begin")
+      v-image(:config="currentItemBackgroundConfig.mid")
+      v-image(:config="currentItemBackgroundConfig.end")
+
+    v-image(
+      v-if="type === FieldType.SELECTED"
+      :config="selectedItemFrameConfig"
+    )
+    v-image(
+      v-else
+      :config="itemFrameConfig"
+    )
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType, toRefs } from '@nuxtjs/composition-api'
+import { useMarkdownEnhancer } from "~/assets/hooks/useMarkdownEnhancer"
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String as PropType<FieldType>,
+      required: true
+    },
+    position: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(props) {
+    const { type, position } = toRefs(props)
+
+    const baseItemFrameConfig = useMarkdownEnhancer(`0/66:1-382/53;T`)
+    const baseSelectedItemFrameConfig = useMarkdownEnhancer(`0/66:55-382/53;T`)
+    const baseSelectedItemBackgroundConfig = {
+      begin: useMarkdownEnhancer(`0/6-73:309-380/3-55;T`),
+      mid: useMarkdownEnhancer(`0/380-73:312-380/3-55;T`),
+      end: useMarkdownEnhancer(`0/6-73:315-380/3-55;T`)
+    }
+    const baseCurrentItemBackgroundConfig = {
+      begin: useMarkdownEnhancer(`0/6-73:319-380/3-55;T`),
+      mid: useMarkdownEnhancer(`0/380-73:322-380/3-55;T`),
+      end: useMarkdownEnhancer(`0/6-73:325-380/3-55;T`)
+    }
+
+    const itemFrameConfig = computed(() => ({ ...baseItemFrameConfig, x: 55, y: 167 + 73 * position.value }))
+    const selectedItemFrameConfig = computed(() => ({ ...baseSelectedItemFrameConfig, x: 55, y: 167 + 73 * position.value }))
+    const selectedItemBackgroundConfig = computed(() => ({
+      begin: { ...baseSelectedItemBackgroundConfig.begin, x: 51, y: 163 + 73 * position.value },
+      mid: { ...baseSelectedItemBackgroundConfig.mid, x: 57, y: 163 + 73 * position.value },
+      end: { ...baseSelectedItemBackgroundConfig.end, x: 437, y: 163 + 73 * position.value }
+    }))
+    const currentItemBackgroundConfig = computed(() => ({
+      begin: { ...baseCurrentItemBackgroundConfig.begin, x: 51, y: 163 + 73 * position.value },
+      mid: { ...baseCurrentItemBackgroundConfig.mid, x: 57, y: 163 + 73 * position.value },
+      end: { ...baseCurrentItemBackgroundConfig.end, x: 437, y: 163 + 73 * position.value }
+    }))
+
+    return {
+      itemFrameConfig,
+      selectedItemFrameConfig,
+      selectedItemBackgroundConfig,
+      currentItemBackgroundConfig,
+      FieldType
+    }
+  },
+})
+
+export enum FieldType {
+  DEFAULT = 'default',
+  SELECTED = 'selected',
+  EQUIPPED = 'equipped'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- components/Shop/Levelup.vue
+++ components/Shop/Levelup.vue
@@ -1,13 +1,21 @@
 <template lang="pug">
-
+  v-group
+    shop-field(:type="FieldType.DEFAULT" :position="1")
+    shop-field(:type="FieldType.SELECTED" :position="2")
+    shop-field(:type="FieldType.DEFAULT" :position="3")
+    shop-field(:type="FieldType.EQUIPPED" :position="4")
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import { FieldType } from "~/components/Shop/Field.vue"
 
 export default defineComponent({
   setup() {
 
+    return {
+      FieldType
+    }
   },
 })
 </script>
