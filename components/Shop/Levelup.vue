8f7a3ed7768f2008c65f0d65fe12151d35c6e350
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-12 17:40:11 +0300

    :construction: Prepare site for attribute generation

--- components/Shop/Levelup.vue
+++ components/Shop/Levelup.vue
@@ -4,45 +4,57 @@
     util-text(:config="descriptionConfig")
 
     shop-field(
-      v-for="n in 3"
-      :type="selected.items.includes(n) ? FieldType.SELECTED : FieldType.DEFAULT"
+      v-for="n in 4"
+      :key="`levelup-field-${n}`"
+      :type="entries.selected.includes(n) ? FieldType.SELECTED : FieldType.DEFAULT"
       :position="n"
       :click="() => handleClick(n)"
     )
-    shop-field(:type="FieldType.EQUIPPED" :position="4")
 
     v-image(:config="acceptButtonConfig")
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from '@nuxtjs/composition-api'
+import { computed, defineComponent, onMounted, reactive } from '@nuxtjs/composition-api'
 import { FieldType } from "~/components/Shop/Field.vue"
 import { useAcceptButton } from "~/components/Shop/useAcceptButton"
 import { useTitle } from "~/components/Shop/useTitle"
+import { useAccessor } from "~/assets/hooks"
+import { Attribute } from "~/assets/entities/attributes/Attribute"
 
 export default defineComponent({
   setup() {
+    const { character } = useAccessor()
     const { titleConfig, descriptionConfig } = useTitle( 'Level Up!', 'Choose 2 skills to improve', '#26ff00')
 
-    const selected = reactive<{ items: number[] }>({ items: [] })
+    const entries = reactive({
+      selected: [] as number[],
+      attributes: [] as Attribute[] 
+    })
 
     const acceptButtonConfig = computed(() => ({
       ...useAcceptButton().value,
-      opacity: selected.items.length === 2 ? 1 : .5
+      opacity: entries.selected.length === 2 ? 1 : .5
     }))
 
     const handleClick = (index: number) => {
-      if (selected.items.includes(index)) {
-        selected.items.splice(selected.items.indexOf(index), 1)
+      if (entries.selected.includes(index)) {
+        entries.selected.splice(entries.selected.indexOf(index), 1)
         return
       }
 
-      selected.items.push(index)
-      if (selected.items.length > 2) selected.items.shift()
+      entries.selected.push(index)
+      if (entries.selected.length > 2) entries.selected.shift()
     }
+    
+    onMounted(() => {
+      for (let i=0; i<4; i++) {
+
+      }
+    })
 
     return {
-      selected,
+      entries,
       FieldType,
       acceptButtonConfig,
       titleConfig,
