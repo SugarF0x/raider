021a90c469e44653017af1e8fda20b4e165609b1
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-19 14:31:26 +0300

    :recycle: Move enumKeys to utils

--- store/character/getters.ts
+++ store/character/getters.ts
@@ -1,17 +1,14 @@
 import { getterTree } from "typed-vuex"
 import { state } from "./"
 import { TileType } from "~/assets/entities/tiles"
-import { Attribute, AttributeType } from "~/assets/entities/attributes"
+import { AttributeType } from "~/assets/entities/attributes"
 import { BASE_ARMOR_VALUE, BASE_HEALTH_VALUE } from "~/assets/consts/balance"
-
-function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
-  return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
-}
+import { getEnumKeys } from "~/assets/utils"
 
 export const getters = getterTree(state, {
   totalAttributes: state => {
     const attributes: Partial<Record<AttributeType, number>> = {}
-    for (const type of enumKeys(AttributeType)) {
+    for (const type of getEnumKeys(AttributeType)) {
       const fixedType = type.toLowerCase() as AttributeType
       switch(fixedType) {
         // TODO: add special filters for strength, dexterity, vitality & luck since they can be also present on equipment
