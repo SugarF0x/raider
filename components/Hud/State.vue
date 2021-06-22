bc305f2c1fb6a6fb52f25fa24a0f8bb1f488c9e1
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-22 12:09:57 +0300

    :construction: Bind sword damage to total attributes

--- components/Hud/State.vue
+++ components/Hud/State.vue
@@ -2,7 +2,7 @@
   v-group
     util-text(:config="{text: `+${enemyPower}`, x: 130, y: 597, width: 50}")
     util-text(:config="{text: `${armor}/${totalArmor}`, x: 197, y: 597, width: 50, fill: 'lightblue'}")
-    util-text(:config="{text: '+'+totalAttack, x: 270, y: 597, width: 50, fill: 'lightgray'}")
+    util-text(:config="{text: '+'+swordDamage, x: 270, y: 597, width: 50, fill: 'lightgray'}")
 </template>
 
 <script lang="ts">
@@ -14,14 +14,14 @@ export default defineComponent({
     const { character, instance } = useAccessor()
     const armor = computed(() => character.armor)
     const totalArmor = computed(() => character.totalArmor)
-    const totalAttack = computed(() => character.swordPower)
+    const swordDamage = computed(() => character.totalAttributes.damage)
     const enemyPower = computed(() => instance.enemyPower)
 
     return {
       armor,
       totalArmor,
       enemyPower,
-      totalAttack,
+      swordDamage,
     }
   },
 })
--- store/character/getters.ts
+++ store/character/getters.ts
@@ -4,6 +4,7 @@ import { TileType } from "~/assets/entities/tiles"
 import { AttributeType } from "~/assets/entities/attributes"
 import { BASE_HEALTH_VALUE } from "~/assets/consts/balance"
 import { getEnumKeys } from "~/assets/utils"
+import { ItemType } from "~/assets/entities/items"
 
 export const getters = getterTree(state, {
   totalAttributes: state => {
@@ -12,9 +13,16 @@ export const getters = getterTree(state, {
       const fixedType = type.toLowerCase() as AttributeType
       switch(fixedType) {
         // TODO: add special filters for strength, dexterity, vitality & luck since they can be also present on equipment
+        case AttributeType.DAMAGE: {
+          let damage = 0
+          damage += state.attributes.find(attr => attr.type === AttributeType.DAMAGE)?.level || 0
+          damage += state.items.find(item => item.type === ItemType.WEAPON)?.stat.level || 0
+          attributes[AttributeType.DAMAGE] = damage
+          break
+        }
         case AttributeType.DEFENSE: {
           let defense = 0
-          defense += (state.attributes.find(attr => attr.type === AttributeType.DEFENSE)?.level || 0)
+          defense += state.attributes.find(attr => attr.type === AttributeType.DEFENSE)?.level || 0
           defense += state.items.filter(item => item.stat.type === AttributeType.DEFENSE).reduce((acc, item) => acc + item.stat.level, 0)
           attributes[AttributeType.DEFENSE] = defense
           break
@@ -32,13 +40,12 @@ export const getters = getterTree(state, {
       .filter((tile: any) => tile.type === TileType.SWORD)
       .length
 
-    const SWORD_POWER = getters.swordPower
+    const SWORD_POWER = getters.totalAttributes.damage
     const STRENGTH = 1 + getters.totalAttributes.strength
 
     return STRENGTH + SWORD_POWER*swords
   },
-  totalHealth: (state, getters) => BASE_HEALTH_VALUE + getters.totalAttributes.health*15 + getters.totalAttributes.vitality*5,
-  swordPower: (state, getters) => 1 + getters.totalAttributes.damage
+  totalHealth: (state, getters) => BASE_HEALTH_VALUE + getters.totalAttributes.health*15 + getters.totalAttributes.vitality*5
 })
 
 export default getters
